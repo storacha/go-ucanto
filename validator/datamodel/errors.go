7592786118ce9d3093b54e88096bb9b9aaa14e5a
hannahhoward <hannah@hannahhoward.net>
2024-07-29 18:56:51 -0700

    feat(validator): add error models
    add all errors from JS Ucanto

---
+++ validator/datamodel/errors.go
@@ -0,0 +1,63 @@
+package datamodel
+
+import (
+	// for go:embed
+	_ "embed"
+	"fmt"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/schema"
+)
+
+//go:embed errors.ipldsch
+var errorsch []byte
+
+var (
+	errorTypeSystem *schema.TypeSystem
+)
+
+func init() {
+	ts, err := ipld.LoadSchemaBytes(errorsch)
+	if err != nil {
+		panic(fmt.Errorf("failed to load IPLD schema: %s", err))
+	}
+	errorTypeSystem = ts
+}
+
+func InvalidAudienceType() schema.Type {
+	return errorTypeSystem.TypeByName("InvalidAudience")
+}
+
+type Delegation struct {
+	Audience string
+}
+
+type InvalidAudienceModel struct {
+	Name       *string
+	Audience   string
+	Delegation Delegation
+	Message    string
+	Stack      *string
+}
+
+type ExpiredModel struct {
+	Name      *string
+	Message   string
+	ExpiredAt int64
+	Stack     *string
+}
+
+func ExpiredType() schema.Type {
+	return errorTypeSystem.TypeByName("Expired")
+}
+
+type NotValidBeforeModel struct {
+	Name    *string
+	Message string
+	ValidAt int64
+	Stack   *string
+}
+
+func NotValidBeforeType() schema.Type {
+	return errorTypeSystem.TypeByName("NotValidBefore")
+}
