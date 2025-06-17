b5d25a75ae9de3f4c2d9fd3ba6006d049ea8f716
Alan Shaw <alan@storacha.network>
2025-06-17 16:51:28 +0100

    refactor: constructor names

--- server/server.go
+++ server/server.go
@@ -92,7 +92,7 @@ func NewServer(id principal.Signer, options ...Option) (ServerView, error) {
 
 	codec := cfg.codec
 	if codec == nil {
-		codec = car.NewCARInboundCodec()
+		codec = car.NewInboundCodec()
 	}
 
 	canIssue := cfg.canIssue
