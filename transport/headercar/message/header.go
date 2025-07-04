ba0adde8258d19dc424a2a3e846dd211a42e0e2c
Alan Shaw <alan@storacha.network>
2025-07-04 11:36:28 +0100

    refactor: allow use without ucanto server

---
+++ transport/headercar/message/header.go
@@ -0,0 +1,65 @@
+package message
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+
+	"github.com/multiformats/go-multibase"
+	"github.com/storacha/go-ucanto/core/car"
+	"github.com/storacha/go-ucanto/core/dag/blockstore"
+	"github.com/storacha/go-ucanto/core/ipld"
+	"github.com/storacha/go-ucanto/core/message"
+)
+
+// AgentMessageHeader is the default name of the HTTP header.
+var AgentMessageHeader = "X-Agent-Message"
+
+// EncodeHeader encodes a [message.AgentMessage] as a HTTP header string.
+func EncodeHeader(msg message.AgentMessage) (string, error) {
+	data := car.Encode([]ipld.Link{msg.Root().Link()}, msg.Blocks())
+
+	r, w := io.Pipe()
+	go func() {
+		gz := gzip.NewWriter(w)
+		_, err := io.Copy(gz, data)
+		gz.Close()
+		w.CloseWithError(err)
+	}()
+
+	var b bytes.Buffer
+	_, err := b.ReadFrom(r)
+	if err != nil {
+		return "", fmt.Errorf("reading encoded CAR: %w", err)
+	}
+
+	h, err := multibase.Encode(multibase.Base64, b.Bytes())
+	if err != nil {
+		return "", fmt.Errorf("multibase encoding: %w", err)
+	}
+
+	return h, nil
+}
+
+// DecodeHeader decodes a [message.AgentMessage] from a HTTP header string.
+func DecodeHeader(h string) (message.AgentMessage, error) {
+	_, data, err := multibase.Decode(h)
+	if err != nil {
+		return nil, fmt.Errorf("multibase decoding X-Agent-Message header: %w", err)
+	}
+	gz, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("creating gzip reader: %w", err)
+	}
+	defer gz.Close()
+	roots, blocks, err := car.Decode(gz)
+	if err != nil {
+		return nil, fmt.Errorf("decoding CAR: %w", err)
+	}
+	bstore, err := blockstore.NewBlockReader(blockstore.WithBlocksIterator(blocks))
+	if err != nil {
+		return nil, fmt.Errorf("creating blockstore: %w", err)
+	}
+	return message.NewMessage(roots, bstore)
+}
--- transport/headercar/response/response.go
+++ transport/headercar/response/response.go
@@ -1,17 +1,9 @@
 package response
 
 import (
-	"bytes"
-	"compress/gzip"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
 
-	"github.com/multiformats/go-multibase"
-	"github.com/storacha/go-ucanto/core/car"
-	"github.com/storacha/go-ucanto/core/dag/blockstore"
-	"github.com/storacha/go-ucanto/core/ipld"
 	"github.com/storacha/go-ucanto/core/message"
 	"github.com/storacha/go-ucanto/transport"
 	chmessage "github.com/storacha/go-ucanto/transport/headercar/message"
@@ -19,57 +11,24 @@ import (
 )
 
 func Encode(msg message.AgentMessage, data chmessage.AgentMessageDataStreamer) (transport.HTTPResponse, error) {
-	carBody := car.Encode([]ipld.Link{msg.Root().Link()}, msg.Blocks())
-
-	r, w := io.Pipe()
-	go func() {
-		gz := gzip.NewWriter(w)
-		_, err := io.Copy(gz, carBody)
-		gz.Close()
-		w.CloseWithError(err)
-	}()
-
-	var b bytes.Buffer
-	_, err := b.ReadFrom(r)
+	xAgentMsg, err := chmessage.EncodeHeader(msg)
 	if err != nil {
-		return nil, fmt.Errorf("reading encoded CAR: %w", err)
-	}
-
-	xAgentMsg, err := multibase.Encode(multibase.Base64, b.Bytes())
-	if err != nil {
-		return nil, fmt.Errorf("multibase encoding: %w", err)
+		return nil, fmt.Errorf("encoding %s header: %w", chmessage.AgentMessageHeader, err)
 	}
 
 	body, headers, err := data.Stream(msg)
 	if err != nil {
 		return nil, fmt.Errorf("streaming data: %w", err)
 	}
-	headers.Set("X-Agent-Message", xAgentMsg)
+	headers.Set(chmessage.AgentMessageHeader, xAgentMsg)
 
 	return uhttp.NewHTTPResponse(http.StatusOK, body, headers), nil
 }
 
 func Decode(response transport.HTTPResponse) (message.AgentMessage, error) {
-	msgHdr := response.Headers().Get("X-Agent-Message")
+	msgHdr := response.Headers().Get(chmessage.AgentMessageHeader)
 	if msgHdr == "" {
-		return nil, errors.New("missing X-Agent-Message header in response")
+		return nil, fmt.Errorf("missing %s header in response", chmessage.AgentMessageHeader)
 	}
-	_, data, err := multibase.Decode(msgHdr)
-	if msgHdr == "" {
-		return nil, fmt.Errorf("multibase decoding X-Agent-Message header: %w", err)
-	}
-	gz, err := gzip.NewReader(bytes.NewReader(data))
-	if err != nil {
-		return nil, fmt.Errorf("creating gzip reader: %w", err)
-	}
-	defer gz.Close()
-	roots, blocks, err := car.Decode(gz)
-	if err != nil {
-		return nil, fmt.Errorf("decoding CAR: %w", err)
-	}
-	bstore, err := blockstore.NewBlockReader(blockstore.WithBlocksIterator(blocks))
-	if err != nil {
-		return nil, fmt.Errorf("creating blockstore: %w", err)
-	}
-	return message.NewMessage(roots, bstore)
+	return chmessage.DecodeHeader(msgHdr)
 }
