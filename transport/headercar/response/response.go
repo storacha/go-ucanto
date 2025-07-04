78d70231cf02c38d1f6245a2606292cb4ea64459
Alan Shaw <alan@storacha.network>
2025-07-04 14:57:46 +0100

    fix: response status for failures

--- transport/headercar/response/response.go
+++ transport/headercar/response/response.go
@@ -1,11 +1,13 @@
 package response
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/storacha/go-ucanto/core/message"
+	"github.com/storacha/go-ucanto/core/result"
 	"github.com/storacha/go-ucanto/transport"
 	hcmsg "github.com/storacha/go-ucanto/transport/headercar/message"
 	uhttp "github.com/storacha/go-ucanto/transport/http"
@@ -34,6 +36,33 @@ func Encode(msg message.AgentMessage, options ...EncodeOption) (transport.HTTPRe
 		return nil, fmt.Errorf("encoding %s header: %w", hcmsg.AgentMessageHeader, err)
 	}
 
+	status := http.StatusOK
+	receipts := msg.Receipts()
+	if len(receipts) != 1 {
+		return nil, errors.New("unexpected number of receipts in response")
+	}
+	rcpt, ok, err := msg.Receipt(receipts[0])
+	if !ok {
+		return nil, fmt.Errorf("missing receipt in agent message: %s", receipts[0])
+	}
+	if err != nil {
+		return nil, fmt.Errorf("getting receipt: %s: %w", receipts[0], err)
+	}
+	_, x := result.Unwrap(rcpt.Out())
+	if x != nil {
+		status = http.StatusInternalServerError
+		n, err := x.LookupByString("name")
+		if err != nil {
+			name, err := n.AsString()
+			if err != nil {
+				switch name {
+				case "Unauthorized":
+					status = http.StatusUnauthorized
+				}
+			}
+		}
+	}
+
 	var headers http.Header
 	var body io.Reader
 	if opts.bodyProvider != nil {
@@ -52,7 +81,7 @@ func Encode(msg message.AgentMessage, options ...EncodeOption) (transport.HTTPRe
 	}
 	headers.Set(hcmsg.AgentMessageHeader, xAgentMsg)
 
-	return uhttp.NewHTTPResponse(http.StatusOK, body, headers), nil
+	return uhttp.NewHTTPResponse(status, body, headers), nil
 }
 
 func Decode(response transport.HTTPResponse) (message.AgentMessage, error) {
