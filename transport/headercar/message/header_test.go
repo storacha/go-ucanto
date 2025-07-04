3167bb325a50f5dc7ffc0ecd9fc0fb995a6aa788
Alan Shaw <alan@storacha.network>
2025-07-04 11:48:58 +0100

    test: add roundtrip test

---
+++ transport/headercar/message/header_test.go
@@ -0,0 +1,36 @@
+package message
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/storacha/go-ucanto/core/invocation"
+	"github.com/storacha/go-ucanto/core/message"
+	"github.com/storacha/go-ucanto/testing/fixtures"
+	"github.com/storacha/go-ucanto/ucan"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRoundTrip(t *testing.T) {
+	inv, err := invocation.Invoke(
+		fixtures.Alice,
+		fixtures.Service,
+		ucan.NewCapability(
+			"test/invoke",
+			fixtures.Alice.DID().String(),
+			ucan.NoCaveats{},
+		),
+	)
+	require.NoError(t, err)
+
+	msg, err := message.Build([]invocation.Invocation{inv}, nil)
+	require.NoError(t, err)
+
+	s, err := EncodeHeader(msg)
+	require.NoError(t, err)
+
+	fmt.Printf("X-Agent-Message: %s (%d bytes)\n", s, len(s))
+
+	_, err = DecodeHeader(s)
+	require.NoError(t, err)
+}
