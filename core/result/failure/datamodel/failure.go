1c34a9cb0cf71e58cdf6823e21a68e622dcde918
Alan Shaw <alan@storacha.network>
2025-08-05 16:56:14 +0200

    feat: add utility to bind a node to a FailureModel

--- core/result/failure/datamodel/failure.go
+++ core/result/failure/datamodel/failure.go
@@ -20,6 +20,10 @@ type FailureModel struct {
 	Stack   *string
 }
 
+func (f FailureModel) Error() string {
+	return f.Message
+}
+
 func (f *FailureModel) ToIPLD() (ipld.Node, error) {
 	return ucanipld.WrapWithRecovery(f, typ)
 }
@@ -40,4 +44,36 @@ func FailureType() schema.Type {
 
 func Schema() []byte {
 	return failureSchema
+}
+
+// Bind binds the IPLD node to a [datamodel.FailureModel]. This works around
+// IPLD requiring data to match the schema _exactly_.
+//
+// Note: the IPLD node is expected to be a map kind, with a "message" key and
+// optionally a "name" and "stack" (all values strings). If none of these are
+// true then you get back a nil value [datamodel.FailureModel].
+func Bind(n ipld.Node) FailureModel {
+	f := FailureModel{}
+	nn, err := n.LookupByString("name")
+	if err == nil {
+		name, err := nn.AsString()
+		if err == nil {
+			f.Name = &name
+		}
+	}
+	mn, err := n.LookupByString("message")
+	if err == nil {
+		msg, err := mn.AsString()
+		if err == nil {
+			f.Message = msg
+		}
+	}
+	sn, err := n.LookupByString("stack")
+	if err == nil {
+		stack, err := sn.AsString()
+		if err == nil {
+			f.Stack = &stack
+		}
+	}
+	return f
 }
