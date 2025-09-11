836c655f1ea0e7dbad5e87e28269b77f69a1b4ec
Alan Shaw <alan@storacha.network>
2025-09-11 16:22:11 +0100

    fix: less change

--- client/retrieval/connnection.go
+++ client/retrieval/connnection.go
@@ -205,60 +205,61 @@ func sendPartialInvocations(ctx context.Context, inv invocation.Invocation, conn
 		case <-ctx.Done():
 			return nil, ctx.Err()
 		default:
-			input, err := newPartialInvocationMessage(inv.Link(), part)
-			if err != nil {
-				return nil, fmt.Errorf("building message: %w", err)
-			}
-
-			req, err := conn.Codec().Encode(input)
-			if err != nil {
-				return nil, fmt.Errorf("encoding message: %w", err)
-			}
-
-			res, err := conn.Channel().Request(ctx, req)
-			if err != nil {
-				return nil, fmt.Errorf("sending message: %w", err)
-			}
-
-			if res.Status() == http.StatusPartialContent || res.Status() == http.StatusOK {
-				return res, nil
-			}
-
-			// if still too big, then fail
-			if res.Status() == http.StatusRequestHeaderFieldsTooLarge {
-				return nil, errors.New("invocation is too big to send in HTTP headers")
-			}
-
-			if res.Status() != http.StatusNotExtended {
-				return nil, fmt.Errorf("unexpected status code: %d", res.Status())
-			}
-
-			bodyReader := res.Body()
-			body, err := io.ReadAll(bodyReader)
-			if err != nil {
-				bodyReader.Close()
-				return nil, fmt.Errorf("reading not extended body: %w", err)
-			}
-			if err = bodyReader.Close(); err != nil {
-				return nil, fmt.Errorf("closing response body: %w", err)
-			}
-
-			var model rdm.MissingProofsModel
-			err = json.Decode(body, &model, rdm.MissingProofsType())
-			if err != nil {
-				return nil, fmt.Errorf("decoding body: %w", err)
-			}
-			if len(model.Proofs) == 0 {
-				return nil, errors.New("server did not include missing proofs in response")
-			}
-
-			p, ok := parts[model.Proofs[0].String()]
-			if !ok {
-				return nil, fmt.Errorf("missing proof not found or was already sent: %s", model.Proofs[0].String())
-			}
-			part = p
-			delete(parts, p.Link().String())
 		}
+
+		input, err := newPartialInvocationMessage(inv.Link(), part)
+		if err != nil {
+			return nil, fmt.Errorf("building message: %w", err)
+		}
+
+		req, err := conn.Codec().Encode(input)
+		if err != nil {
+			return nil, fmt.Errorf("encoding message: %w", err)
+		}
+
+		res, err := conn.Channel().Request(ctx, req)
+		if err != nil {
+			return nil, fmt.Errorf("sending message: %w", err)
+		}
+
+		if res.Status() == http.StatusPartialContent || res.Status() == http.StatusOK {
+			return res, nil
+		}
+
+		// if still too big, then fail
+		if res.Status() == http.StatusRequestHeaderFieldsTooLarge {
+			return nil, errors.New("invocation is too big to send in HTTP headers")
+		}
+
+		if res.Status() != http.StatusNotExtended {
+			return nil, fmt.Errorf("unexpected status code: %d", res.Status())
+		}
+
+		bodyReader := res.Body()
+		body, err := io.ReadAll(bodyReader)
+		if err != nil {
+			bodyReader.Close()
+			return nil, fmt.Errorf("reading not extended body: %w", err)
+		}
+		if err = bodyReader.Close(); err != nil {
+			return nil, fmt.Errorf("closing response body: %w", err)
+		}
+
+		var model rdm.MissingProofsModel
+		err = json.Decode(body, &model, rdm.MissingProofsType())
+		if err != nil {
+			return nil, fmt.Errorf("decoding body: %w", err)
+		}
+		if len(model.Proofs) == 0 {
+			return nil, errors.New("server did not include missing proofs in response")
+		}
+
+		p, ok := parts[model.Proofs[0].String()]
+		if !ok {
+			return nil, fmt.Errorf("missing proof not found or was already sent: %s", model.Proofs[0].String())
+		}
+		part = p
+		delete(parts, p.Link().String())
 	}
 }
 
