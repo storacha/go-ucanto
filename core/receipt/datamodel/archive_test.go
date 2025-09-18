33bb863f85d6e6f028ebfeeeba60a7951ead6bc8
Vicente Olmedo <vicente@storacha.network>
2025-09-18 11:35:20 +0200

    add receipt archive schema

---
+++ core/receipt/datamodel/archive_test.go
@@ -0,0 +1,34 @@
+package datamodel_test
+
+import (
+	"testing"
+
+	"github.com/ipfs/go-cid"
+	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	"github.com/storacha/go-ucanto/core/ipld/block"
+	"github.com/storacha/go-ucanto/core/ipld/codec/cbor"
+	"github.com/storacha/go-ucanto/core/ipld/hash/sha256"
+	adm "github.com/storacha/go-ucanto/core/receipt/datamodel"
+)
+
+func TestArchiveEncodeDecode(t *testing.T) {
+	l := cidlink.Link{Cid: cid.MustParse("bafkreiem4twkqzsq2aj4shbycd4yvoj2cx72vezicletlhi7dijjciqpui")}
+	m0 := adm.ArchiveModel{
+		UcanReceipt0_9_1: l,
+	}
+	mblk, err := block.Encode(&m0, adm.ArchiveType(), cbor.Codec, sha256.Hasher)
+	if err != nil {
+		t.Fatalf("encoding archive model: %s", err)
+	}
+
+	m1 := adm.ArchiveModel{}
+	err = block.Decode(mblk, &m1, adm.ArchiveType(), cbor.Codec, sha256.Hasher)
+	if err != nil {
+		t.Fatalf("decoding agent message: %s", err)
+	}
+
+	d1 := m1.UcanReceipt0_9_1
+	if d1.String() != l.String() {
+		t.Fatalf("failed round trip link")
+	}
+}
